a17fd277500ce871fbb855f6cb9a38c3cd1dd89b
Thalia-guedes <thalia.guedes@zup.com.br>
2023-04-27 14:29:10 -0300

    Atividade

---
+++ src/numeros.java
@@ -0,0 +1,12 @@
+public class numeros {
+
+        public static void main(String[] args) {
+            int numero = 101;
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(numero);
+                numero++;
+            }
+        }
+
+}
---
+++ src/tabuada.java
@@ -0,0 +1,16 @@
+public class tabuada {
+    public static void main (String [] args) {
+        int tabuada = 8;
+        int i = 0;
+
+        for(i = 1; i <=10; i ++) {
+
+            System.out.print("Tabuada do ");
+            System.out.print( i + "x" + tabuada + "=");
+            System.out.println(i * tabuada);
+        }
+
+
+
+    }
+}
